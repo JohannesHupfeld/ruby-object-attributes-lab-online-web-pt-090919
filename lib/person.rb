e974421c3c0805de6c0bef1b55099baf6ecd44b5
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-10-20 03:23:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,19 @@
 class Person
   
-  def name=(persons_name)
-    @name = persons_name
+  def name=(name)
+    @name = name
   end
   
   def name
     @name
   end
-  
-  def job = (job)
+
+  def job=(job)
     @job = job
-  end
-  
-  def job
+  end 
+
+  def job 
     @job
-  end
- end
+  end 
+  
+end
